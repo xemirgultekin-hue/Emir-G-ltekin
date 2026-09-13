a47a45b3bcfec49d0d6459d4db0aa68fed233010
xemirgultekin-hue <xemirgultekin@gmail.com>
2026-09-13 20:57:36 +0300

    i18n ve sitemap yapilandirmasi eklendi

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
 
 export default defineConfig({
   site: "https://emirgultekin.com",
+  i18n: {
+    defaultLocale: "tr",
+    locales: ["tr"],
+  },
   integrations: [
     sitemap({
       filter: (page) => !page.includes("/admin"),
     }),
   ],
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [remarkCollapse, { test: "Table of contents" }],
-    ],
-  },
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
